e0844317781d57f8711ce3a2a54485f83c267bad
riyan-135 <riyanrahmansyah@gmail.com>
2023-11-29 13:00:43 +0700

    done

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+      <GetApi />
+  </div>
+</template>
+
+<script>
+import GetApi from './components/getApi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GetApi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
